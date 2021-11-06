c669dc66453fc6c828be64213f6985249528f222
DrSmugleaf <DrSmugleaf@users.noreply.github.com>
2021-11-06 20:21:36 +0100

    Add code to create links from old and new map images

---
+++ MapDiffBot/Arguments.cs
@@ -0,0 +1,26 @@
+﻿using CommandLine;
+
+namespace MapDiffBot
+{
+    public class Arguments
+    {
+        [Option("uploaded", Required = true, HelpText = "An array of image links.")]
+        public string Uploaded { get; set; } = default!;
+        
+        [Option("maps", Required = true, HelpText = "An array of map files.")]
+        public string Maps { get; set; } = default!;
+        
+        // Example: https://spacestation14.io/map-diff-viewer/
+        [Option("url", Required = true, HelpText = "Url to append ?old and ?new query parameters on.")]
+        public string DifferUrl { get; set; } = default!;
+
+        [Option("baseCommit", Required = true, HelpText = "Commit hash of the base branch.")]
+        public string BaseCommit { get; set; } = default!;
+        
+        [Option("repoName", Required = true, HelpText = "Repository's full name in the format {owner}/{repo}.")]
+        public string RepoName { get; set; } = default!;
+        
+        [Option("mapImageDirectory", Required = true, HelpText = "Path to the directory that contains old map images..")]
+        public string MapImageDirectory { get; set; } = default!;
+    }
+}
--- MapDiffBot/Program.cs
+++ MapDiffBot/Program.cs
@@ -1,6 +1,66 @@
-﻿namespace MapDiffBot
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using CommandLine;
+
+namespace MapDiffBot
 {
     internal static class Program
     {
+        private const string GithubContentUrl = "https://raw.githubusercontent.com/";
+        
+        private static void Main(string[] args)
+        {
+            Parser.Default
+                .ParseArguments<Arguments>(args)
+                .WithParsed(Comment)
+                .WithNotParsed(_ => Environment.Exit(2));
+        }
+
+        private static string[] ParseArray(string str)
+        {
+            return str
+                .Remove(0, 1)
+                .Remove(str.Length - 1, 1)
+                .Split(',')
+                .Select(s => s.Remove(0, 1).Remove(s.Length - 1, 1))
+                .ToArray();
+        }
+
+        private static void Comment(Arguments args)
+        {
+            var images = ParseArray(args.Uploaded);
+            var maps = ParseArray(args.Maps);
+            args.DifferUrl += args.DifferUrl.EndsWith('/') ? string.Empty : '/';
+
+            var message = new StringBuilder($"{maps.Length} maps modified.");
+            
+            for (var i = 0; i < maps.Length; i++)
+            {
+                var mapPath = maps[i];
+                var mapName = Path.GetFileNameWithoutExtension(mapPath);
+                var mapDirectory = new FileInfo(mapPath).Directory!.FullName;
+                var jsonPath = $"{mapDirectory}{Path.DirectorySeparatorChar}{mapName}.json";
+
+                string? oldImageLink = null;
+                if (File.Exists(jsonPath))
+                {
+                    var json = JsonDocument.Parse(jsonPath).RootElement;
+
+                    if (json.GetProperty("oldLink").GetString() is { } oldLink)
+                    {
+                        oldImageLink =  Uri.EscapeDataString(oldLink);
+                    }
+                }
+
+                var newImageLink = Uri.EscapeDataString(images[i]);
+                oldImageLink ??= $"{GithubContentUrl}{args.RepoName}/{args.BaseCommit}/{args.MapImageDirectory}";
+                var url = $"{args.DifferUrl}?old={oldImageLink}&new={newImageLink}";
+
+                message.AppendLine($"[{mapName}]({url})");
+            }
+        }
     }
 }
